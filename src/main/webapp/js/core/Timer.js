c4387c0f2f5b769234015ce1d5066b9c9efb7708
marbernh <marcus.bernhard92@gmail.com>
2013-10-07 16:44:14 +0200

    Daniel grejar, ahahahahahahahahahaha

--- src/main/webapp/js/core/Timer.js
+++ src/main/webapp/js/core/Timer.js
@@ -7,35 +7,53 @@
  *
  * @author Vera Lomander
  */
-var myCounter = new Countdown({  
-    seconds:30,  // number of seconds to count down
-    updateStatus: function(seconds){console.log(seconds);}, // callback for each second
-    counterEnd: function(){ alert('counter ended!');} // Utöka
-});
 
-myCounter.start();
+var count=30;
 
-function Countdown(options){
-    var timer,
-    seconds = options.seconds,
-    updateStatus = options.UpdateStatus;
-    
-    function decreaseCounter(){
-        updateStatus(seconds);
-        if (seconds ===0) {
-            counterEnd();
-        }
-        seconds--;
-    };
-    
-    this.start = function (){
-        cleatInterval(timer);
-        timer=0;
-        seconds = options.seconds;
-        timer = setInterval(decreaseCounter, 1000);
-    };
-    
-    this.stop = function(){
-        clearInterval(timer);
-    };
+var counter=setInterval(timer, 1000); //1000 will  run it every 1 second
+
+function timer()
+{
+  count=count-1;
+  if (count <= 0)
+  {
+     clearInterval(counter);
+     //counter ended, do something here
+     return;
+  }
+
+  document.getElementById("timer").innerHTML=count + " secs"; 
 }
+
+//var myCounter = new Countdown({  
+//    seconds:30,  // number of seconds to count down
+//    updateStatus: function(seconds){console.log(seconds);}, // callback for each second
+//    counterEnd: function(){ alert('counter ended!');} // Utöka
+//});
+//
+//myCounter.start();
+//
+//function Countdown(options){
+//    var timer,
+//    seconds = options.seconds,
+//    updateStatus = options.UpdateStatus;
+//    
+//    function decreaseCounter(){
+//        updateStatus(seconds);
+//        if (seconds ===0) {
+//            counterEnd();
+//        }
+//        seconds--;
+//    };
+//    
+//    this.start = function (){
+//        cleatInterval(timer);
+//        timer=0;
+//        seconds = options.seconds;
+//        timer = setInterval(decreaseCounter, 1000);
+//    };
+//    
+//    this.stop = function(){
+//        clearInterval(timer);
+//    };
+//}
