312ad7cf7cf53b9d29e1980969658dbedc3fd7f2
marbernh <marcus.bernhard92@gmail.com>
2013-10-16 14:56:27 +0200

    Gameoversida, about

---
+++ src/test/java/web/proj/barbosa/quiz/TestQuiz.java
@@ -0,0 +1,34 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package web.proj.barbosa.quiz;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *Test for OO-model Quiz
+ * @author Marcus
+ */
+public class TestQuiz {
+    
+    public TestQuiz() {
+    }
+    
+    @Before
+    public void before() {
+        
+    }
+    
+    @Test
+    public void testQuiz(){
+        
+    }
+    
+
+}
