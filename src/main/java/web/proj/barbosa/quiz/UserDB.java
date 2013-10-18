982241dfb3babcd702ba5fd27e31fb8134c8d58c
husnjak <husnjak@ed3358-12.ed3358.studat.a.1.hb.cdal.chalmers.se>
2013-10-18 15:28:48 +0200

    Filip fixar gamesPlayed i UserDB

--- src/main/java/web/proj/barbosa/quiz/UserDB.java
+++ src/main/java/web/proj/barbosa/quiz/UserDB.java
@@ -4,11 +4,7 @@
  */
 package web.proj.barbosa.quiz;
 
-import javax.persistence.*;
-import javax.xml.bind.annotation.XmlAccessType;
-import javax.xml.bind.annotation.XmlAccessorType;
-import javax.xml.bind.annotation.XmlElement;
-import javax.xml.bind.annotation.XmlRootElement;
+import javax.persistence.Entity;
 import web.proj.barbosa.quiz.superbean.utils.AbstractEntity;
 
 /**
@@ -18,12 +14,11 @@ import web.proj.barbosa.quiz.superbean.utils.AbstractEntity;
  * @author Iron-Maven
  */
 @Entity
-@XmlRootElement
-@XmlAccessorType(XmlAccessType.PROPERTY)
 public class UserDB extends AbstractEntity {
 
     private String userName, password;
     private int topGameScore = 0;
+    private int gamesPlayed;
 
     public UserDB() {
     }
@@ -31,20 +26,20 @@ public class UserDB extends AbstractEntity {
     public UserDB(String userName, String password) {
         this.userName = userName;
         this.password = password;
+        gamesPlayed = 0;
     }
 
     public UserDB(Long id, String userName, String password) {
         super(id);
         this.userName = userName;
         this.password = password;
+        gamesPlayed = 0;
     }
 
-    @XmlElement
     public String getUserName() {
         return userName;
     }
 
-    @XmlElement
     public String getPassword() {
         return password;
     }
@@ -53,9 +48,14 @@ public class UserDB extends AbstractEntity {
         if (score > topGameScore) {
             topGameScore = score;
         }
+        gamesPlayed++;
     }
 
     public int getTopGameScore() {
         return topGameScore;
+    }
+    
+    public int getGamesPlayed(){
+        return gamesPlayed;
     }
 }
