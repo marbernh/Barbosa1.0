05ceb20a71b46263227c5130515d1c75e4f26366
marbernh <marcus.bernhard92@gmail.com>
2013-10-20 20:57:52 +0200

    Fixat test, testar samma som innan

--- src/test/java/web/proj/barbosa/quiz/TestQuiz.java
+++ src/test/java/web/proj/barbosa/quiz/TestQuiz.java
@@ -23,27 +23,23 @@ public class TestQuiz {
     @Before
     public void before() {
         
-//    }
-//    
-//    @Test
-//    public void testQuiz(){
-//        GameEngine e = new GameEngine();
-//        e.newGame();
-//        Guess g = new Guess(e);
-//        String rightAnswer1 = e.getAnswer();
-//        assertTrue(g.validate(rightAnswer1).equals("Your answer is correct"));
-//        assertTrue(e.getScore() == 15);
-//        assertTrue(g.validate("fel svar 1").equals("Your answer is wrong"));
-//        assertTrue(e.getLife() == 2);
-//        assertTrue(g.validate("fel svar 2").equals("Your answer is wrong"));
-//        // Gameover after three wrong answers
-//        assertTrue(g.validate("fel svar 3").equals("gameOver"));
-//        
-//        // New game because gameover 
-//        String rightAnswer2 = e.getAnswer();
-//        assertFalse(rightAnswer1.equals(rightAnswer2));
-//        
     }
     
-    
+    @Test
+    public void testQuiz(){
+        Quiz q = new Quiz();
+        q.newGame();
+        String rightAnswer1 = q.getAnswer();
+        assertTrue(q.validate(rightAnswer1).equals("Your answer is correct"));
+        assertTrue(q.getScore() == 15);
+        assertTrue(q.validate("fel svar 1").equals("Your answer is wrong"));
+        assertTrue(q.getLife() == 2);
+        assertTrue(q.validate("fel svar 2").equals("Your answer is wrong"));
+        // Gameover after three wrong answers
+        assertTrue(q.validate("fel svar 3").equals(""));
+        
+        // New game because gameover 
+        String rightAnswer2 = q.getAnswer();
+        assertFalse(rightAnswer1.equals(rightAnswer2));   
+    }
 }
