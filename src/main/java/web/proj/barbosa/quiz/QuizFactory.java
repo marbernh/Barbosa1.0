e026c0bb4a64f654788b4fb8b174cf6c85440ec8
husnjak <husnjak@student.chalmers.se>
2013-10-20 17:02:22 +0200

    Speelare

--- src/main/java/web/proj/barbosa/quiz/QuizFactory.java
+++ src/main/java/web/proj/barbosa/quiz/QuizFactory.java
@@ -13,7 +13,7 @@ import java.util.Random;
 public class QuizFactory {
 
     
-//    private static PlayerRegister reg = (PlayerRegister) PlayerRegister.newInstance("quiz_pu");
+//    private static PlayerRegister reg = (PlayerRegister) PlayerRegister.newInstance("quiz_pu"); 
     private static Random scoreMaker = new Random();
     
     public static IQuiz getQuiz(Boolean fill){
@@ -24,12 +24,13 @@ public class QuizFactory {
     }
 
     public static void createCompetition() {
+        String[] Names = {"Marcus", "Daniel", "Vera", "Anton", "Sofia", "Patricia", "Ants", "Barbosa", "Jack Sparrow"};
         PlayerRegister reg = (PlayerRegister) PlayerRegister.newInstance("quiz_pu");
         if (reg.getAll().isEmpty()) {
             Player u;
-            for (int i = 1; i <= 10; i++) {
-                u = new Player("Player nr: " + i, "password");
-                u.update(scoreMaker.nextInt(750));
+            for (int i = 0; i <= 8; i++) {
+                u = new Player(Names[i], "password");
+                u.update(scoreMaker.nextInt(800));
                 reg.add(u);
             }
             u = new Player("Filip", "pass");
